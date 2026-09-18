0dd1089eee9846da4604f506d331bb4bdb8388aa
sowmya0904 <parepallisaisowmya2k@gmail.com>
2026-09-18 18:01:58 +0530

    multiple pages routing

--- ReactViteProject/Ecommerce-Project/src/App.jsx
+++ ReactViteProject/Ecommerce-Project/src/App.jsx
@@ -1,11 +1,13 @@
+import {Routes, Route} from 'react-router'
 import './App.css'
 import { HomePage } from './Pages/HomePage'
 function App() {
 
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/checkout" element={<div>Checkout</div>} />
+    </Routes>
   )
 }
 
