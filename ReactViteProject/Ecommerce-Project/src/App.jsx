f307ce6d6575f1c35fd55859b43aaa3cfc56f03b
sowmya0904 <parepallisaisowmya2k@gmail.com>
2026-09-18 18:09:58 +0530

    checkout page created

--- ReactViteProject/Ecommerce-Project/src/App.jsx
+++ ReactViteProject/Ecommerce-Project/src/App.jsx
@@ -1,12 +1,13 @@
 import {Routes, Route} from 'react-router'
 import './App.css'
 import { HomePage } from './Pages/HomePage'
+import {CheckOut} from './Pages/CheckOut'
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/checkout" element={<div>Checkout</div>} />
+      <Route path="/checkout" element={<CheckOut/>} />
     </Routes>
   )
 }
