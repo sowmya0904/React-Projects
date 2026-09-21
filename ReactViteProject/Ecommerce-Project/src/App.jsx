9f68a585d880d40d671c73e4803b5b7199833cfb
sowmya0904 <parepallisaisowmya2k@gmail.com>
2026-09-21 12:59:06 +0530

    orders and tracking pages setup done

--- ReactViteProject/Ecommerce-Project/src/App.jsx
+++ ReactViteProject/Ecommerce-Project/src/App.jsx
@@ -2,12 +2,16 @@ import {Routes, Route} from 'react-router'
 import './App.css'
 import { HomePage } from './Pages/HomePage'
 import {CheckOut} from './Pages/CheckOut'
+import {OrdersPage} from './Pages/OrdersPage'
+import {TrackingPage} from './Pages/TrackingPage'
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/checkout" element={<CheckOut/>} />
+      <Route path="/orders" element={<OrdersPage/>} />
+      <Route path="/tracking" element={<TrackingPage/>} />
     </Routes>
   )
 }
